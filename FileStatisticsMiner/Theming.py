0f51c06957a5c613a87c026889febfafa40fc9e8
Matt Eland <matt.eland@gmail.com>
2021-12-22 16:41:23 -0500

    Work on visualizations

--- FileStatisticsMiner/Theming.py
+++ FileStatisticsMiner/Theming.py
@@ -1,4 +1,9 @@
 import plotly.express as px
 
 theme = px.colors.qualitative.Prism
+theme_diverging = px.colors.diverging.Picnic_r
+theme_diverging_r = px.colors.diverging.Picnic
+theme_sequential = px.colors.sequential.Agsunset
+theme_continuous='balance'
+
 template = 'plotly_dark'
